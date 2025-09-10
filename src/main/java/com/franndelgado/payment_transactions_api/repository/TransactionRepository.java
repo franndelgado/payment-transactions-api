e8db67889d71372306fc022d0301325f26f17aad
María Francisca Delgado <delgadofrancisca22@gmail.com>
2025-09-10 12:36:39 -0300

    Update TransactionRepository.java

--- src/main/java/com/franndelgado/payment_transactions_api/repository/TransactionRepository.java
+++ src/main/java/com/franndelgado/payment_transactions_api/repository/TransactionRepository.java
@@ -1,9 +1,11 @@
 package com.franndelgado.payment_transactions_api.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import com.franndelgado.payment_transactions_api.entity.Transaction;
 
+@Repository
 public interface TransactionRepository extends JpaRepository<Transaction, String> {
 
 }
