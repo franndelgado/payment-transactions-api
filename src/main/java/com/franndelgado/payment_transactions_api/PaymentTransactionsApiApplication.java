782de5dfed4f417b88faa7495d0f8e4d7932ea68
María Francisca Delgado <delgadofrancisca22@gmail.com>
2025-09-10 09:57:29 -0300

    Configuración inicial de Spring Boot

---
+++ src/main/java/com/franndelgado/payment_transactions_api/PaymentTransactionsApiApplication.java
@@ -0,0 +1,13 @@
+package com.franndelgado.payment_transactions_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PaymentTransactionsApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PaymentTransactionsApiApplication.class, args);
+	}
+
+}
