2fa78f6902d77ddefad47bea020a4af597e98c63
María Francisca Delgado <delgadofrancisca22@gmail.com>
2025-09-10 14:43:41 -0300

    Update Transaction.java

--- src/main/java/com/franndelgado/payment_transactions_api/entity/Transaction.java
+++ src/main/java/com/franndelgado/payment_transactions_api/entity/Transaction.java
@@ -2,26 +2,34 @@ package com.franndelgado.payment_transactions_api.entity;
 
 import java.math.BigDecimal;
 import java.time.Instant ;
+import java.util.UUID;
 
 import com.franndelgado.payment_transactions_api.enums.TransactionStatus;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Entity
 @Table(name = "transactions")
 @Getter
 @Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class Transaction {
 
     @Id
-    private String transaction_id;
+    private String transaction_id = UUID.randomUUID().toString();
     private String user_id;
     private BigDecimal amount;
     private String currency;
+    @Enumerated(EnumType.STRING)
     private TransactionStatus status;
     private Instant created_at;
     private String bank_code;
