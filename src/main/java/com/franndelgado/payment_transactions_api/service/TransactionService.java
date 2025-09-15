2d86e6e35f841e7d8fa97dc54eb14d6e9dda381e
María Francisca Delgado <delgadofrancisca22@gmail.com>
2025-09-15 09:25:18 -0300

    Update TransactionService.java

--- src/main/java/com/franndelgado/payment_transactions_api/service/TransactionService.java
+++ src/main/java/com/franndelgado/payment_transactions_api/service/TransactionService.java
@@ -2,11 +2,12 @@ package com.franndelgado.payment_transactions_api.service;
 
 import java.util.List;
 
-import com.franndelgado.payment_transactions_api.dto.TransactionDTO;
+import com.franndelgado.payment_transactions_api.dto.TransactionRequestDTO;
+import com.franndelgado.payment_transactions_api.dto.TransactionResponseDTO;
 import com.franndelgado.payment_transactions_api.enums.TransactionStatus;
 
 public interface TransactionService {
-    TransactionDTO createTransaction(TransactionDTO transactionDTO);
+    TransactionResponseDTO createTransaction(TransactionRequestDTO transactionDTO);
     TransactionStatus getTransactionStatus(String transaction_id);
-    List<TransactionDTO> getApprovedTransactionsByUserId(String user_id);
+    List<TransactionResponseDTO> getApprovedTransactionsByUserId(String user_id);
 }
